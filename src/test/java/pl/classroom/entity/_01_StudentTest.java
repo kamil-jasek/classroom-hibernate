91ff4d2201ea4584f1aad7e6798b95317c64d7a0
Kamil Jasek <kamil.jasek@dbschenker.com>
2020-09-20 08:30:51 +0200

    fixed student tests

--- src/test/java/pl/classroom/entity/_01_StudentTest.java
+++ src/test/java/pl/classroom/entity/_01_StudentTest.java
@@ -6,12 +6,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.Serializable;
-import java.math.BigInteger;
 import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import org.junit.Test;
 import pl.classroom.entity.Student.Gender;
@@ -39,16 +34,22 @@ public final class _01_StudentTest extends BaseEntityTest {
         // given - example name:
         var name = "Jan";
         // create some students
-
+        saveAndFlush(new Student("Jan", "Kowalski", DateUtil.from(LocalDate.of(2000, 1, 1)), Gender.MALE));
+        saveAndFlush(new Student("Adam", "Kowalski", DateUtil.from(LocalDate.of(2000, 1, 1)), Gender.MALE));
+        saveAndFlush(new Student("Wojtek", "Jan", DateUtil.from(LocalDate.of(2000, 1, 1)), Gender.MALE));
+        saveAndFlush(new Student("jan", "Kwiatek", DateUtil.from(LocalDate.of(2000, 1, 1)), Gender.MALE));
 
         // when - create hql to find students with given "name" (query should check first and lastname)
-        List<Student> results = new ArrayList<>();
-
+        List<Student> results = getSession()
+            .createQuery("from Student s where upper(s.firstName) like :name or upper(s.lastname) like :name", Student.class)
+            .setParameter("name", name)
+            .getResultList();
 
         // then - check if results contains students with "name"
         assertFalse(results.isEmpty());
         assertTrue(results.stream().allMatch(student -> student
             .getFullName().toLowerCase().contains(name.toLowerCase())));
+        assertEquals(3, results.size());
     }
 
     @Test
@@ -56,12 +57,18 @@ public final class _01_StudentTest extends BaseEntityTest {
         // given - example gender
         var gender = Gender.FEMALE;
         // create some students
-
+        saveAndFlush(new Student("Jan", "Kowalski", DateUtil.from(LocalDate.of(2000, 1, 1)), Gender.FEMALE));
+        saveAndFlush(new Student("Adam", "Kowalski", DateUtil.from(LocalDate.of(2000, 1, 1)), Gender.MALE));
+        saveAndFlush(new Student("Wojtek", "Jan", DateUtil.from(LocalDate.of(2000, 1, 1)), Gender.FEMALE));
+        saveAndFlush(new Student("jan", "Kwiatek", DateUtil.from(LocalDate.of(2000, 1, 1)), Gender.MALE));
 
         // when - create hql which counts students by gender
-        BigInteger count = BigInteger.ZERO;
+        var count = getSession()
+            .createQuery("select count(s) from Student s where s.gender = :gender", Long.class)
+            .setParameter("gender", gender)
+            .getSingleResult();
 
         // then - check count
-        assertTrue(count.compareTo(BigInteger.ZERO) > 0);
+        assertEquals(2, count.intValue());
     }
 }
